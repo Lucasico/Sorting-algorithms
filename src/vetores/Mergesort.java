b2b017cacc34675d67b967f5cc849dc2625264d1
Lucas <lucassantoscrfbezerra@gmail.com>
2019-10-15 19:22:29 -0300

    Add classe merge, com respectivo método de ordenação e calculo de tempo de execução

---
+++ src/vetores/Mergesort.java
@@ -0,0 +1,83 @@
+
+package vetores;
+/**
+ *
+ * @author lucas
+ */
+public class Mergesort {
+    
+   
+    void merge(int vetor[], int esquerdo, int meio, int direito) 
+    { 
+        
+        int n1 = meio - esquerdo + 1; 
+        int n2 = direito - meio; 
+       
+        int L[] = new int [n1]; 
+        int R[] = new int [n2]; 
+      
+        for (int i=0; i<n1; ++i) 
+            L[i] = vetor[esquerdo + i]; 
+        for (int j=0; j<n2; ++j) 
+            R[j] = vetor[meio + 1+ j]; 
+       
+        int i = 0, j = 0; 
+        
+        int k = esquerdo; 
+        while (i < n1 && j < n2) 
+        { 
+            if (L[i] <= R[j]) 
+            { 
+                vetor[k] = L[i]; 
+                i++; 
+            } 
+            else
+            { 
+                vetor[k] = R[j]; 
+                j++; 
+            } 
+            k++; 
+        } 
+        
+        while (i < n1) 
+        { 
+            vetor[k] = L[i]; 
+            i++; 
+            k++; 
+        } 
+        
+        while (j < n2) 
+        { 
+            vetor[k] = R[j]; 
+            j++; 
+            k++; 
+        } 
+    }
+     
+    void sort(int vetor[], int esquerdo, int direito) 
+    { 
+        if (esquerdo < direito) 
+        { 
+            
+            int meio = (esquerdo+direito)/2; 
+           
+            sort(vetor, esquerdo, meio); 
+            sort(vetor , meio+1, direito); 
+            
+            merge(vetor, esquerdo, meio, direito); 
+        } 
+    }
+    void temposExecucao(long antesDaOrdenacao, long finalDaOrdenacao){
+        
+        final long diferencaTime = (finalDaOrdenacao - antesDaOrdenacao);
+     
+        System.out.println(" --------- Tempos de execução do MERGESORT---------");
+        System.out.println("> Tempo em nanosegundos antes da ordenação = " + antesDaOrdenacao);
+        System.out.println("> Tempo em nanosegundos após a ordenação = " + finalDaOrdenacao);
+        System.out.println("> Tempo de diferença entre Time final e Time inicial = " + diferencaTime);
+        System.out.println("OBS.: algoritmo preparado para ordenar na forma crescente");
+        System.out.println("");
+                
+    }
+    
+}
